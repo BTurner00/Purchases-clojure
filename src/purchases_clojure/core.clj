0d790325fcd00e8d9e6c24b84eca59125c5eab80
Ben <bhturne@g.clemson.edu>
2016-07-06 14:34:46 -0400

    turned into web app, data displayed in table format

--- src/purchases_clojure/core.clj
+++ src/purchases_clojure/core.clj
@@ -1,24 +1,58 @@
 (ns purchases-clojure.core
-  (:require [clojure.string :as str])
+  (:require [clojure.string :as str]
+            [compojure.core :as c]
+            [ring.adapter.jetty :as j]
+            [hiccup.core :as h])
   (:gen-class))
-
-(defn -main []
+(defn read-purchases []
   (let [purchases (slurp "purchases.csv")
         purchases (str/split-lines purchases)
-        purchases (map #(str/split % #",") purchases)
+        purchases (map (fn [line] (str/split line #","))
+                    purchases)
         header (first purchases)
         purchases (rest purchases)
-        purchases (map #(zipmap header %)
-                    purchases)
-        _ (println "Enter a category:  Furniture, Alcohol, Toiletries, Shoes, Food, Jewelry")
-        
-        input (read-line)
-        purchases (filter #(= input (get % "category"))
-                       purchases)
-        text (pr-str purchases)]
-        
-        
-        (spit "filtered_purchases.edn" text)))
+        purchases (map (fn [line] (zipmap header line))
+                    purchases)]
+    purchases))
+       
+
+(defn purchases-html [purchases] 
+  [:html
+   [:body
+    [:table 
      
+      (map (fn [purchase]
+             [:tr
+              [:td (:style "width:50%") (str(get purchase "customer_id"))]
+              [:td (str(get purchase "date"))]
+              [:td (str(get purchase "credit_card"))]
+              [:td (str(get purchase "cvv"))]
+              [:td (str(get purchase "category"))]])
+              
+               
+                 
+          purchases)]]])
+
+
+(defn filter-by-category [purchases category]
+  (filter (fn [purchase]
+            (= category (get purchase "category")))
+          purchases))
+     
+(c/defroutes app
+  (c/GET "/:category{.*}" [category]
+    (let [purchases (read-purchases)
+          purchases (if (= 0 (count category))
+                     purchases
+                     (filter-by-category purchases category))]                      
+      (h/html (purchases-html purchases))))) 
+
+
+(defonce server (atom nil))
+
+(defn -main []
+  (if @server
+    (.stop @server))
+  (reset! server (j/run-jetty app {:port 3000 :join? false}))) 
   
   
